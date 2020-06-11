903eff802f8db8f708c28ea7600df6805eddb2c4
Philippe Ombredanne <pombredanne@nexb.com>
2020-06-12 01:05:01 +0200

    Add wrapper for image fetcher from registry
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

---
+++ src/conan/fetch/__init__.py
@@ -0,0 +1,20 @@
+# Copyright (c) nexB Inc. and others. All rights reserved.
+# http://nexb.com and https://github.com/nexB/conan/
+#
+# This software is licensed under the Apache License version 2.0.#
+#
+# You may not use this software except in compliance with the License.
+# You may obtain a copy of the License at:
+#     http://apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from .docker_image import DockerRegistry
+from .docker_image import normalize_docker_registry_name
+from .docker_image import DOCKER_REGISTRY
