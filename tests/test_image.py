e40748ab5f2452736e7d0bc0271ce6e46b739389
Philippe Ombredanne <pombredanne@nexb.com>
2021-03-03 16:16:42 +0100

    Removed unused test files
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- tests/test_image.py
+++ tests/test_image.py
@@ -16,35 +16,14 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 from os import path
-from os import listdir
-from unittest.case import expectedFailure
 
 from commoncode.testcase import FileBasedTesting
 
 from container_inspector.image import Image
-from container_inspector.image import Layer
 from container_inspector.image import flatten_images
 
 from utilities import check_expected
 from utilities import clean_image
-from utilities import clean_layer
-
-
-class TestLegacyLayer(FileBasedTesting):
-    test_data_dir = path.join(path.dirname(__file__), 'data')
-
-    def test_Layer(self):
-        Layer()
-
-    def test_load_layer(self):
-        test_dir = self.get_test_loc('layers/layers')
-        expected_dir = self.get_test_loc('layers/expected')
-        for layer_id in listdir(test_dir):
-            layer_dir = path.join(test_dir, layer_id)
-            layer = Layer.from_layer_dir(layer_dir)
-            expected = path.join(expected_dir, layer_id + '.expected.json')
-            result = clean_layer(layer).to_dict()
-            check_expected(result, expected, regen=False)
 
 
 class TestImages(FileBasedTesting):
@@ -53,7 +32,7 @@ class TestImages(FileBasedTesting):
     def test_Image(self):
         Image()
 
-    def test_from_tarball(self):
+    def test_Image_get_images_from_tarball(self):
         test_tarball = self.get_test_loc('repos/imagesv11.tar')
         extract_dir = self.get_temp_dir()
         expected = path.join(self.get_test_loc('repos'), 'imagesv11.tar.expected.json')
@@ -61,7 +40,7 @@ class TestImages(FileBasedTesting):
             for i in Image.get_images_from_tarball(test_tarball, extract_dir, force_extract=True)]
         check_expected(result, expected, regen=False)
 
-    def test_from_dir(self):
+    def test_Image_get_images_from_dir(self):
         test_tarball = self.get_test_loc('repos/imagesv11.tar')
         test_dir = self.extract_test_tar(test_tarball)
         expected = path.join(self.get_test_loc('repos'), 'imagesv11.tar.expected.json')
@@ -69,7 +48,7 @@ class TestImages(FileBasedTesting):
             for i in Image.get_images_from_dir(test_dir)]
         check_expected(result, expected, regen=False)
 
-    def test_from_hello_world(self):
+    def test_Image_get_images_from_dir_from_hello_world(self):
         test_arch = self.get_test_loc('repos/hello-world.tar')
         test_dir = self.extract_test_tar(test_arch)
         expected = path.join(self.get_test_loc('repos'), 'hello-world.tar.registry.expected.json')
@@ -77,7 +56,7 @@ class TestImages(FileBasedTesting):
             for i in Image.get_images_from_dir(test_dir)]
         check_expected(result, expected, regen=False)
 
-    def test_flatten_images(self):
+    def test_Image_get_images_from_dir_then_flatten_images(self):
         test_arch = self.get_test_loc('repos/hello-world.tar')
         test_dir = self.extract_test_tar(test_arch)
         expected = path.join(self.get_test_loc('repos'), 'hello-world.tar.flatten.expected.json')
@@ -85,12 +64,33 @@ class TestImages(FileBasedTesting):
         result = list(flatten_images(images))
         check_expected(result, expected, regen=False)
 
-    # rare and not yet supported
-    @expectedFailure
-    def test_images_with_direct_at_root_layerid_dot_tar_tarball(self):
+    def test_Image_get_images_from_dir_with_direct_at_root_layerid_dot_tar_tarball(self):
         test_arch = self.get_test_loc('repos/imagesv11_with_tar_at_root.tar')
         test_dir = self.extract_test_tar(test_arch)
         expected = path.join(self.get_test_loc('repos'), 'imagesv11_with_tar_at_root.tar.registry.expected.json')
         result = [clean_image(i).to_dict()
             for i in Image.get_images_from_dir(test_dir)]
         check_expected(result, expected, regen=False)
+
+    def test_Image_get_images_from_dir_with_verify(self):
+        test_arch = self.get_test_loc('repos/hello-world.tar')
+        test_dir = self.extract_test_tar(test_arch)
+        list(Image.get_images_from_dir(test_dir, verify=True))
+
+    def test_Image_get_images_from_dir_with_anotations(self):
+        test_arch = self.get_test_loc('repos/images.tar.gz')
+        test_dir = self.extract_test_tar(test_arch)
+        expected = path.join(self.get_test_loc('repos'), 'images.tar.gz.expected.json')
+        result = [clean_image(i).to_dict()
+            for i in Image.get_images_from_dir(test_dir)]
+        check_expected(result, expected, regen=False)
+
+    def test_Image_get_images_from_dir_with_verify_fails_if_invalid_checksum(self):
+        test_arch = self.get_test_loc('repos/images.tar.gz')
+        test_dir = self.extract_test_tar(test_arch)
+        try:
+            list(Image.get_images_from_dir(test_dir, verify=True))
+            self.fail('Exception not raised')
+        except AssertionError as e:
+            assert str(e).startswith('Layer SHA256')
+ 
