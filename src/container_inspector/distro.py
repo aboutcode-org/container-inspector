2adf842b133193cdaa744169e612c8b832fd8329
Jono Yang <jyang@nexb.com>
2024-05-16 12:15:58 -0700

    Use parse_os_release from commoncode
        * Remove parse_os_release and tests from container-inspector
    
    Signed-off-by: Jono Yang <jyang@nexb.com>

--- src/container_inspector/distro.py
+++ src/container_inspector/distro.py
@@ -8,12 +8,13 @@
 
 import logging
 import os
-import shlex
 from os import path
 
 import attr
+from commoncode.distro import parse_os_release
 
 from container_inspector import rootfs
+
 
 TRACE = False
 logger = logging.getLogger(__name__)
@@ -469,39 +470,6 @@ class Distro(object):
         if TRACE: logger.debug(f'merge: merged data: {existing!r}')
 
         return type(self)(**existing)
-
-
-def parse_os_release(location):
-    """
-    Return a mapping built from an os-release-like file at `location`.
-
-    See https://www.linux.org/docs/man5/os-release.html
-
-    $ cat /etc/os-release
-    NAME="Ubuntu"
-    VERSION="16.04.6 LTS (Xenial Xerus)"
-    ID=ubuntu
-    ID_LIKE=debian
-    PRETTY_NAME="Ubuntu 16.04.6 LTS"
-    VERSION_ID="16.04"
-    HOME_URL="http://www.ubuntu.com/"
-    SUPPORT_URL="http://help.ubuntu.com/"
-    BUG_REPORT_URL="http://bugs.launchpad.net/ubuntu/"
-    VERSION_CODENAME=xenial
-    UBUNTU_CODENAME=xenial
-
-    Note the /etc/lsb-release file has the same format, but different tags.
-    """
-    with open(location) as osrl:
-        lines = (line.strip() for line in osrl)
-        lines = (
-            line.partition('=') for line in lines
-            if line and not line.startswith('#')
-        )
-        return {
-            key.strip(): ''.join(shlex.split(value))
-            for key, _, value in lines
-        }
 
 
 def get_debian_details():
